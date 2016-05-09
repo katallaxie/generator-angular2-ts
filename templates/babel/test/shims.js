ead17cce124ed7f8bcf64f4bc0ca22cd6d722fc7
Sebastian Döll <sebastian@pixelmilk.com>
2016-05-09 08:32:27 +0200

    ~ removing some deps

--- templates/babel/test/shims.js
+++ templates/babel/test/shims.js
@@ -1,4 +1,4 @@
-// these are the shims
+// testing shims
 import 'es6-shim';
 import 'zone.js';
-import 'reflect-metadata';
+import 'reflect-metadata'; // loads some of the shims that are in the browser
--- templates/typescript/karma.conf.js
+++ templates/typescript/karma.conf.js
@@ -11,33 +11,31 @@ module.exports = config => {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: [
       'jspm',
-      'mocha',
-      'chai',
-      'chai-as-promised',
-      'sinon-chai'
+      'jasmine'
     ],
 
     // list of plugins
     plugins: [
       'karma-jspm',
-      'karma-mocha',
-      'karma-chai',
-      'karma-chai-plugins',
+      'karma-jasmine',
       'karma-phantomjs-launcher',
       'karma-chrome-launcher',
-      'karma-babel-preprocessor'
+      'karma-babel-preprocessor',
+      'karma-coverage',
+      'karma-junit-reporter'
     ],
 
     // jspm
     jspm: {
-      config: 'jspm.config.js',
-      loadFiles: ['src/app/app.js', 'src/app/**/*.spec.js'],
+      config: 'config.js',
+      loadFiles: ['test/shims.js', 'src/app/**/*.spec.js'],
       serveFiles: ['src/app/**/*.!(uat)+(js)']
     },
 
     // proxies
     proxies: {
       '/src/app': '/base/src/app',
+      '/test': '/base/test',
       '/jspm_packages/' : '/base/jspm_packages/'
     },
 
@@ -45,7 +43,7 @@ module.exports = config => {
     preprocessors: {
       // source files, that you wanna generate coverage for - do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      'src/**/*.js': ['babel']
+      'src/**/*.!(spec)+(js)': ['babel', 'coverage']
     },
 
     // transpile with babel since the coverage reporter throws error on ES6 syntax
@@ -58,9 +56,7 @@ module.exports = config => {
     },
 
     // list of files / patterns to load in the browser
-    files: [
-      'node_modules/babel-polyfill/dist/polyfill.js'
-    ],
+    files: [],
 
     // list of files to exclude
     exclude: [],
@@ -68,7 +64,38 @@ module.exports = config => {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: [ 'progress', 'junit', 'coverage' ],
+
+    // junit report, for jenkins
+    junitReporter: {
+      outputDir: '.',
+      outputFile: 'test-results.xml',
+       useBrowserName: false
+    },
+
+    // report coverage in different formats
+    coverageReporter: {
+      // this should also include formats for jenkins
+      type: 'html',
+      dir: 'reports/coverage/',
+      reporters: [
+        {
+          type: 'html',
+          subdir: 'html'
+        },
+        {
+          type: 'lcov',
+          subdir: 'lcov'
+        },
+        // reporters supporting the `file` property, use `subdir` to directly
+        // output them in the `dir` directory
+        {
+          type: 'cobertura',
+          subdir: '../../',
+          file: 'coverage.xml'
+        }
+      ]
+    },
 
 
     // web server port
