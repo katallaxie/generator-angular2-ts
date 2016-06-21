1b71790a201717e61e56b3ea38327cab6b350ba2
Sebastian Döll <sebastian@pixelmilk.com>
2016-06-21 19:42:40 +0200

    ~ fixing an issue with testing in typescript

--- templates/typescript/karma.conf.js
+++ templates/typescript/karma.conf.js
@@ -15,13 +15,10 @@ module.exports = config => {
 
     // list of plugins
     plugins: [
-      'karma-jspm',
       'karma-jasmine',
       'karma-phantomjs-launcher',
-      'karma-chrome-launcher',
-      'karma-babel-preprocessor',
-      'karma-coverage',
-      'karma-junit-reporter'
+      'karma-typescript-preprocessor',
+      'karma-chrome-launcher'
     ],
 
     // proxies
@@ -33,15 +30,23 @@ module.exports = config => {
     preprocessors: {
       // source files, that you wanna generate coverage for - do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      'src/**/*.!(spec)+(js)': ['babel', 'coverage']
+      '**/*.ts': ['typescript']
     },
 
-    // transpile with babel since the coverage reporter throws error on ES6 syntax
-    babelPreprocessor: {
+    typescriptPreprocessor: {
+      // options passed to the typescript compiler
       options: {
-        presets: ['es2015', 'stage-0'],
-        sourceMap: 'inline',
-        plugins: ['transform-decorators-legacy']
+        sourceMap: false, // (optional) Generates corresponding .map file.
+        target: 'es5', // (optional) Specify ECMAScript target version: 'ES3' (default), or 'ES5'
+        module: 'commonjs', // (optional) Specify module code generation: 'commonjs' or 'amd'
+        noImplicitAny: true, // (optional) Warn on expressions and declarations with an implied 'any' type.
+        noResolve: false, // (optional) Skip resolution and preprocessing.
+        removeComments: true, // (optional) Do not emit comments to output.
+        concatenateOutput: false // (optional) Concatenate and emit output to single file. By default true if module option is omited, otherwise false.
+      },
+      // transforming the filenames
+      transformPath: function(path) {
+        return path.replace(/\.ts$/, '.js');
       }
     },
 
@@ -55,21 +60,24 @@ module.exports = config => {
       'node_modules/systemjs/dist/system.src.js',
       'node_modules/reflect-metadata/Reflect.js',
 
-      {pattern: 'node_modules/@angular/**/*.js', included: false, watched: false, served: true},
       {pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false, served: true},
-      {pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false, served: true},
-      {pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false, served: true},
+      {pattern: 'node_modules/@angular/**/*.js', included: false, watched: false, served: true},
+      {pattern: 'node_modules/babel-polyfill/**/*.js', included: false, watched: false},
+      {pattern: 'node_modules/plugin-typescript/**/*.js', included: false, watched: false},
       {pattern: 'node_modules/moment/**/*.js', included: false, watched: false, served: true},
       {pattern: 'node_modules/ng2-bootstrap/**/*.js', included: false, watched: false, served: true},
+      {pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false, served: true},
+      {pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false, served: true},
       {pattern: 'node_modules/symbol-observable/**/*.js', included: false, watched: false},
-      {pattern: 'node_modules/babel-polyfill/**/*.js', included: false, watched: false},
       {pattern: 'node_modules/systemjs-plugin-text/**/*.js', included: false, watched: false},
+      {pattern: 'node_modules/ts-helpers/**/*.js', included: false, watched: false},
+      {pattern: 'node_modules/typescript/**/*.js', included: false, watched: false},
 
       // shim for ESx
       'karma.shim.js',
 
       // app
-      {pattern: 'src/app/**/*.js', included: false, watched: false},
+      {pattern: 'src/app/**/*.ts', included: false, watched: false},
 
       // assets
       {pattern: 'src/**/*.html', included: false, watched: true},
@@ -83,38 +91,7 @@ module.exports = config => {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'junit', 'coverage'],
-
-    // junit report, for jenkins
-    junitReporter: {
-      outputDir: '.',
-      outputFile: 'test-results.xml',
-       useBrowserName: false
-    },
-
-    // report coverage in different formats
-    coverageReporter: {
-      // this should also include formats for jenkins
-      type: 'html',
-      dir: 'reports/coverage/',
-      reporters: [
-        {
-          type: 'html',
-          subdir: 'html'
-        },
-        {
-          type: 'lcov',
-          subdir: 'lcov'
-        },
-        // reporters supporting the `file` property, use `subdir` to directly
-        // output them in the `dir` directory
-        {
-          type: 'cobertura',
-          subdir: '../../',
-          file: 'coverage.xml'
-        }
-      ]
-    },
+    reporters: ['progress'],
 
 
     // web server port
@@ -127,7 +104,7 @@ module.exports = config => {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_ERROR,
+    logLevel: config.LOG_WARN,
 
 
     // enable / disable watching file and executing tests whenever any file changes
